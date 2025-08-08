90833bfbcadde0620bb6561a1cec26d71169c11b
Naved Khan <nk9167755768@gmail.com>
2025-08-08 20:34:55 +0530

    Added the binary search and it works properly

--- DSAVisualizerCLI/Algorithem/Searching/Bineary_search_itrative.java
+++ DSAVisualizerCLI/Algorithem/Searching/Bineary_search_itrative.java
@@ -1,4 +1,27 @@
 package DSAVisualizerCLI.Algorithem.Searching;
+import DSAVisualizerCLI.utils.Visualizer;
 
 public class Bineary_search_itrative {
+
+    public static int searchbineary(int[] arr,int target) {
+        int low = 0;
+        int high = arr.length -1;
+
+        while (low<high) {
+            int mid = low + (high - low) / 2;
+
+            Visualizer.printArray(arr,mid,mid+1);
+
+            if (arr[mid] == target) {
+                System.out.println("the targeted value is present in the array");
+                return mid;
+            } else if (arr[mid] < target) {
+                low =  low + 1;
+            }
+            else {
+                high = high - 1;
+            }
+        }
+        return -1;
+    }
 }
--- DSAVisualizerCLI/Main.java
+++ DSAVisualizerCLI/Main.java
@@ -2,6 +2,8 @@ package DSAVisualizerCLI;
 import java.util.Scanner;
 import DSAVisualizerCLI.Algorithem.sorting.bubble_sort;
 import DSAVisualizerCLI.Algorithem.Searching.linear_search;
+import DSAVisualizerCLI.Algorithem.Searching.Bineary_search_itrative;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -14,12 +16,14 @@ public class Main {
 
 
             System.out.println("=== DSA Visualizer ===");
-            System.out.println("Searching algorithm");
+            System.out.println("***Searching algorithm***");
             System.out.println("1. linear search");
 //            System.out.println();
-            System.out.println("Sorting algorithm");
+            System.out.println("***Sorting algorithm***");
             System.out.println("2. bubble sort");
 //            System.out.println();
+            System.out.print("3. Binary sort iterative ");
+            System.out.println("(If you are using Binary search enter sorted elements)");
             System.out.println("0.EXIT");
             System.out.println("Choose the algorithm by given numbers: ");
             int choice = Integer.parseInt(sc.nextLine());
@@ -50,6 +54,12 @@ public class Main {
                     int value = Integer.parseInt(sc.nextLine());
                     System.out.println("\nStarting the linear search visualization");
                     linear_search.searchlinear(arr,value);
+                }
+                case 3-> {
+                    System.out.println("Enter the target value you want to search: ");
+                    int value = Integer.parseInt(sc.nextLine());
+                    System.out.println("\nStarting the Binary search");
+                    Bineary_search_itrative.searchbineary(arr,value);
                 }
             }
 
